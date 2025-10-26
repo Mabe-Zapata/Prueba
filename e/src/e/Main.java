9498d1fa51a26bdb5e78bf6337ea573309d3098e
Mabe Zapata <patylo1970maria@gmail.com>
2025-10-26 15:25:32 -0500

    feat:Se agrego producto

---
+++ e/src/e/Main.java
@@ -0,0 +1,9 @@
+package e;
+
+public class Main {
+    public static void main(String[] args) {
+        Producto producto1 = new Producto("1233221","Leche");
+        System.out.println(producto1.showProducto());
+        
+    }
+}
